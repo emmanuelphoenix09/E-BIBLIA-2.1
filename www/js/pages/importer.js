9028c2afccc941be5b73f358ae501697dcf8619d
emmanuelphoenix09 <141434765+emmanuelphoenix09@users.noreply.github.com>
2026-09-25 18:19:07 +0200

    E-BIBLIA 3.0 : découpler le module de page du moteur principal

--- www/js/pages/importer.js
+++ www/js/pages/importer.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",()=>document.getElementById("btn-process-import")?.addEventListener("click",handleImportJSON));
+document.addEventListener("DOMContentLoaded",()=>document.getElementById("btn-process-import")?.addEventListener("click",()=>window.EBiblia?.handleImportJSON()));
